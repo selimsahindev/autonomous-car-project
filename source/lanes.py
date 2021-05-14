c736dd8d7f2e7e85ad6c9375fe75e3c6b8bc97c1
Selim Şahin <selimsahin.sns@gmail.com>
2021-05-14 19:55:13 +0300

    applied hough line transform to detect lines and average line optimizations carried out

--- source/lanes.py
+++ source/lanes.py
@@ -1,13 +1,67 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3 / 5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    if lines is None:
+        return None
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+def canny(image):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blurred_image = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
+    canny_image = cv2.Canny(blurred_image, 50, 150)
+    return canny_image
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(200, height), (1100, height), (550, 250)]])    
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
 
 test_image = cv2.imread('../image/test_image.jpg')
 
 # Copy our image array into a new variable. Thus, the changes we make will not affect the actual image.
 image = np.copy(test_image)
-grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-blurred_image = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
-canny_image = cv2.Canny(blurred_image, 50, 150)
 
-cv2.imshow('Image', canny_image)
+canny_image = canny(image)
+cropped_image = region_of_interest(canny_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+averaged_lines = average_slope_intercept(image, lines)
+line_image = display_lines(image, averaged_lines)
+image_with_lines = cv2.addWeighted(image, 0.8, line_image, 1, 1)
+
+cv2.imshow('Result', image_with_lines)
 cv2.waitKey(0)
